2ef11dc14065e61bda47f8c0a342fd8eca779ba4
Jisoo Hwang <jeesoo0908@gmail.com>
2024-06-19 00:40:06 +0900

    [Bronze II] Title: 단어의 개수, Time: 304 ms, Memory: 25312 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while(st.hasMoreTokens()){
+            st.nextToken();
+            sum++;
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+}
