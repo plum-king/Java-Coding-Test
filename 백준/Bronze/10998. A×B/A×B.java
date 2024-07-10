335d17c692ada7d4215a1b16ba70c048aedba05c
Jisoo Hwang <jeesoo0908@gmail.com>
2024-07-10 23:57:40 +0900

    [Bronze V] Title: A×B, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(n*m));
+        bw.flush();
+        bw.close();
+    }
+}
