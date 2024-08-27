0379ec80db9d8084a07ff76f8171790415e1f836
Jisoo Hwang <jeesoo0908@gmail.com>
2024-08-27 14:02:16 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9744 ms, Memory: 309004 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -6,6 +6,7 @@ public class Main {
     static ArrayList<Integer> count = new ArrayList<>();
     static int n, m;
     static ArrayList<Integer>[] graph;
+    static Queue<Integer> queue = new LinkedList<>();
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
@@ -27,9 +28,10 @@ public class Main {
             graph[b].add(a);
         }
         for(int i = 0; i < n; i++){
-            dfs(i+1);
+//            dfs(i+1);
+            bfs(i+1);
             count.add(cnt);
-            visited = new boolean[n+1];
+            visited = new boolean[n+1]; // 한 번 방문한 거 체크했으면 다음 접점에 대해 체크하기 위해 초기화
             cnt = 0;
         }
         int max_num = Collections.max(count); // ArrayList 중 가장 큰 수
@@ -41,13 +43,28 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    public static void dfs(int x){
-        visited[x] = true;
-        for(int i: graph[x]){
-            if(!visited[i]){
-                cnt++;
-                dfs(i);
+    public static void bfs(int x){
+        queue.add(x);
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            visited[tmp] = true;
+
+            for(int i: graph[tmp]){
+                if(!visited[i]){
+                    visited[i] = true;
+                    cnt++;
+                    queue.add(i);
+                }
             }
         }
     }
+//    public static void dfs(int x){
+//        visited[x] = true;
+//        for(int i: graph[x]){
+//            if(!visited[i]){
+//                cnt++;
+//                dfs(i);
+//            }
+//        }
+//    }
 }
