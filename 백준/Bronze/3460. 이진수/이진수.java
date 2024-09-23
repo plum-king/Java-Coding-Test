2052c93443174ed28f99108961533b68d8249299
Jisoo Hwang <jeesoo0908@gmail.com>
2024-09-23 20:12:24 +0900

    [Bronze III] Title: 이진수, Time: 96 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.java
+++ 백준/Bronze/3460. 이진수/이진수.java
@@ -6,19 +6,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++){
             int m = Integer.parseInt(br.readLine());
             int j = 0;
             while(m > 0){
                 if(m % 2 != 0){
-                    bw.write(String.valueOf(j)+" ");
+                    sb.append(j + " ");
+//                    bw.write(String.valueOf(j)+" ");
                 }
                 m /= 2;
                 j++;
             }
-            bw.newLine();
+//            bw.newLine();
+            sb.append("\n");
         }
+        bw.write(String.valueOf(sb));
         bw.flush();
         bw.close();
     }
