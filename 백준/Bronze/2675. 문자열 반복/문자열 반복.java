4eb1b8bcfbed33c4d4e40ce1a3209e1239bf052d
Jisoo Hwang <jeesoo0908@gmail.com>
2024-09-25 17:18:31 +0900

    [Bronze II] Title: 문자열 반복, Time: 60 ms, Memory: 11680 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,25 +1,28 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            String s = st.nextToken();
-            String[] s_arr = s.split(""); // 입력 받은 String 값을 "" 기준으로 잘라 배열로 변환
-            String str = "";
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String new_str = "";
 
-            for(int j = 0;  j <s_arr.length; j++){
-                str += s_arr[j].repeat(r); // String의 repeat 함수를 사용하여 같은 문자 반복
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+            int l = 0;
+            for(int j = 1; j <= str.length() * m; j++){
+                new_str += str.charAt(l);
+                if(j % m == 0){
+                    l++;
+                }
             }
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            bw.write(str);
-            bw.newLine();
+            bw.write(new_str+"\n");
+            new_str = "";
             bw.flush();
         }
+        bw.close();
     }
 }
