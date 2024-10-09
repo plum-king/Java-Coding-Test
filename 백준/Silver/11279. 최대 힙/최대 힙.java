23f50ddb95311833a555cb14792f7aa0bbce2857
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-09 15:15:20 +0900

    [Silver II] Title: 최대 힙, Time: 236 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -4,18 +4,21 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int m = Integer.parseInt(br.readLine());
         
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0; i < m; i++){
             int x = Integer.parseInt(br.readLine());
             if(x == 0){
-                if(pq.isEmpty()) System.out.println(x);
-                else System.out.println(pq.poll());
+                if(pq.isEmpty()) sb.append(x);
+                else sb.append(pq.poll());
+                sb.append("\n");
             }
             else{
                 pq.add(x);
             }
         }
+        System.out.println(String.valueOf(sb));
     }
 }
