da65fc9344c80d7021b33655d1568f6772083b7d
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-11 00:37:33 +0900

    [level 1] Title: 모의고사, Time: 0.95 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,24 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         ArrayList<Integer> answer = new ArrayList<>();
-        ArrayList<Integer> cnt = new ArrayList<>();
-        for(int i = 0; i < 3; i++){
-            cnt.add(0);
-        }
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
         int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         for(int i = 0; i < answers.length; i++){
-            if(one[i%5] == answers[i]) cnt.set(0, cnt.get(0)+1);
-            if(two[i%8] == answers[i]) cnt.set(1, cnt.get(1)+1);
-            if(three[i%10] == answers[i]) cnt.set(2, cnt.get(2)+1);
+            if(one[i%5] == answers[i]) cnt1++;
+            if(two[i%8] == answers[i]) cnt2++;
+            if(three[i%10] == answers[i]) cnt3++;
         }
-        int max = Collections.max(cnt);
+        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
 
-        for(int i = 0; i < cnt.size(); i++){
-            if(max == cnt.get(i)) answer.add(i+1);
-        }
+        if(max == cnt1) answer.add(1);
+        if(max == cnt2) answer.add(2);
+        if(max == cnt3) answer.add(3);
         
         int[] an = new int[answer.size()];
         for(int i = 0; i < answer.size(); i++){
