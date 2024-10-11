1a131a6016d685bc4103b08253c6de9d79d22894
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-11 17:46:52 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
-select count(*) as FISH_COUNT, max(length) as MAX_LENGTH, FISH_TYPE
+select count(*) as FISH_COUNT, max(NULLIF(length, 10)) as MAX_LENGTH, FISH_TYPE
 from FISH_INFO
 group by FISH_TYPE
 having avg(NULLIF(length, 10)) >= 33
