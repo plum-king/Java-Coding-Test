8f8954e556224642a0ed05bd117851dd08e231ff
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-12 01:39:10 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,17 +1,17 @@
 -- 코드를 작성해주세요
-with recursive tmp as (
+with recursive tmp as(
     select id, parent_id, 1 as generation
-    from ECOLI_DATA
+    from ecoli_data
     where parent_id is null
     union all
     select ed1.id, ed1.parent_id, tmp.generation+1
     from tmp
-    join ECOLI_DATA ed1
-    on tmp.id = ed1.parent_id
+    join ecoli_data ed1
+    on ed1.parent_id = tmp.id
 )
 
-select count(*) as COUNT, GENERATION
+select count(*) as count, generation
 from tmp
 where id not in (select distinct parent_id from tmp where parent_id is not null)
-group by GENERATION
-order by GENERATION;
+group by generation
+order by generation;
