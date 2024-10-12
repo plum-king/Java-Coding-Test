38cf8f5bf731269eb78717a65276f0f2b5841abe
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-13 00:00:36 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,2 +1,7 @@
 -- 코드를 입력하세요
-SELECT o.ANIMAL_ID, o.NAME FROM ANIMAL_INS i RIGHT JOIN ANIMAL_OUTS o ON i.ANIMAL_ID = o.ANIMAL_ID WHERE i.ANIMAL_ID is NULL and o.ANIMAL_ID is not NULL ORDER BY o.ANIMAL_ID;
+SELECT ao.ANIMAL_ID, ao.name
+from ANIMAL_INS ai
+right join ANIMAL_OUTS ao
+on ai.animal_id = ao.animal_id
+where ai.animal_id is null
+order by ao.ANIMAL_ID;
