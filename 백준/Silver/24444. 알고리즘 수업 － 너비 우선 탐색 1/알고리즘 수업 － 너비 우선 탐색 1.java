d7eb323a08412e7f01696567385abee21c492042
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-15 02:39:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2076 ms, Memory: 111308 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,71 +1,59 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
+public class Main{
     public static int[] visited;
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-    // 순서 저장을 위한 변수
-    public static int count = 0;
-
-    public static void main(String[] args) throws IOException {
+    public static ArrayList<ArrayList<Integer>> array = new ArrayList<>();
+    public static int cnt = 1;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
-
+        
         visited = new int[n+1];
-
-        // 그래프 초기화
-        for(int i = 0; i <= n; i++){
-            graph.add(new ArrayList<Integer>());
+        for(int i = 0; i < n+1; i++){
+            array.add(new ArrayList<>());
         }
-
-        // 그래프에 값 넣기
+        
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-
-        for(int i = 1; i <= n; i++){
-            Collections.sort(graph.get(i));
-        }
-
-        // bfs
-        count++;
-        bfs(r);
-
-        for(int i = 1; i < visited.length; i++){
-            bw.write(String.valueOf(visited[i] + "\n" ));
-        }
-        bw.flush();
-        bw.close();
-    }
-    public static void bfs(int r){
-        Queue<Integer> q = new LinkedList<>(); // queue를 linkedlist로 구현
-        
-        q.add(r);
-        visited[r] = count;
-        
-        while(!q.isEmpty()){
-            int a = q.poll();
             
-            for(int i = 0; i < graph.get(a).size(); i++){
-                int v = graph.get(a).get(i);
-                if(visited[v] != 0) continue;
-                q.offer(v);
-                count++;
-                visited[v] = count;
+            array.get(a).add(b);
+            array.get(b).add(a);
+        }
+        
+        for(int i = 1; i < n+1; i++){
+            Collections.sort(array.get(i));
+        }
+        
+        bfs(r);
+        
+        for(int i = 1; i < n+1; i++){
+            System.out.println(visited[i]);
+        }
+    }
+    
+    public static void bfs(int r){
+        visited[r] = cnt;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(r);
+        
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            
+            for(int i = 0; i < array.get(tmp).size(); i++){
+                int v = array.get(tmp).get(i);
+                if(visited[v] == 0){
+                    cnt++;
+                    visited[v] = cnt;
+                    queue.add(v);
+                }
             }
         }
     }
-
 }
