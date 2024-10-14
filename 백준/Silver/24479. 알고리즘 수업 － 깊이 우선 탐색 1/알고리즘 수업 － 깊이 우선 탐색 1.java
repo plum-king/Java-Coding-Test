a3e647182892040706998ca29153b9127f15bc3f
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-15 02:26:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 2080 ms, Memory: 112992 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,51 +1,50 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
+public class Main{
     public static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
     public static int[] visited;
     public static int cnt = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
-
+        
         visited = new int[n+1];
         
-        for(int i = 0; i <= n; i++){
-            arr.add(new ArrayList<Integer>());
+        for(int i = 0; i < n+1; i++){
+            arr.add(new ArrayList<>());
         }
         
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr.get(u).add(v);
-            arr.get(v).add(u);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.get(a).add(b);
+            arr.get(b).add(a);
         }
-        for(int i = 1; i <= n; i++){
+        
+        for(int i = 1; i < n+1; i++){
             Collections.sort(arr.get(i));
         }
-
         dfs(r);
-        for(int i = 1; i < visited.length; i++){
-            sb.append(visited[i]+"\n");
+        
+        for(int i = 1; i < n+1; i++){
+            System.out.println(visited[i]);
         }
-        System.out.println(String.valueOf(sb));
     }
     
     public static void dfs(int r){
         visited[r] = cnt;
         
         for(int i = 0; i < arr.get(r).size(); i++){
-            int v = arr.get(r).get(i);
-            if(visited[v] == 0){
+            int tmp = arr.get(r).get(i);
+            if(visited[tmp] == 0){
                 cnt++;
-                dfs(v);
+                dfs(tmp);
             }
         }
     }
