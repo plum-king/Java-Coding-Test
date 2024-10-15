3f0197da3ed5b5ca8487e088de932a744293e9ee
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-15 16:51:48 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 13520 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,51 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-    static boolean[][] visited;
-    static int[][] graph;
-    static int n;
-    static int m;
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static int[][] arr;
+    public static int[][] visited;
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
+    public static int n, m;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        graph = new int[n][m];
-        visited = new boolean[n][m];
-
+        arr = new int[n][m];
+        visited = new int[n][m];
+        
         for(int i = 0; i < n; i++){
             String str = br.readLine();
             for(int j = 0; j < m; j++){
-                graph[i][j] = str.charAt(j) - '0';
+                arr[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
             }
         }
-        visited[0][0] = true;
+        
         bfs(0, 0);
-        bw.write(String.valueOf(graph[n-1][m-1]));
-        bw.flush();
-        bw.close();
+        System.out.println(visited[n-1][m-1]);
     }
-
-    static void bfs(int x, int y){
+    public static void bfs(int a, int b){
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-
+        queue.add(new int[]{a, b});
+        visited[a][b] = 1;
+        
         while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            int now_x = now[0];
-            int now_y = now[1];
+            int[] tmp = queue.poll();
+            int x = tmp[0];
+            int y = tmp[1];
+            
             for(int i = 0; i < 4; i++){
-                int next_x = now_x + dx[i];
-                int next_y = now_y + dy[i];
-                if(next_x < n && next_y < m && next_x >= 0 && next_y >= 0) {
-                    if(graph[next_x][next_y] == 1 && !visited[next_x][next_y]){
+                int next_x = x + dx[i];
+                int next_y = y + dy[i];
+                
+                if(next_x >= 0 && next_y >= 0 && next_x < n && next_y < m){
+                    if(visited[next_x][next_y] == 0 && arr[next_x][next_y] == 1){
                         queue.add(new int[]{next_x, next_y});
-                        graph[next_x][next_y] = graph[now_x][now_y] + 1;
-                        visited[next_x][next_y] = true;
+                        visited[next_x][next_y] = visited[x][y] + 1;
                     }
                 }
             }
