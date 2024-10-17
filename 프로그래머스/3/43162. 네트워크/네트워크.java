30401e1b7a7c42fb566e067258c4f1630a7547ff
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-17 19:42:28 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,25 @@
 class Solution {
+    public static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean [] visited = new boolean[computers.length];
-        
-        for(int i = 0; i < computers.length; i++){
-            visited[i] = false; // false로 모두 초기화
-        }
-        
-        for(int i = 0; i < computers.length; i++){
-            if(visited[i] == false){
+        visited = new boolean[computers.length];
+        for(int i = 0; i < visited.length; i++){
+            if(!visited[i]){
                 answer++;
-                dfs(i, visited, computers);
+                dfs(i, computers);
             }
         }
         return answer;
     }
     
-    public static void dfs(int node, boolean[] visited, int[][] c){
-        visited[node] = true;
-        
-        for(int i = 0; i < c.length; i++){
-            if(visited[i] == false && c[node][i] == 1){
-                dfs(i, visited, c);
+    public static void dfs(int x, int[][] computers){
+        visited[x] = true;
+
+        for(int i = 0; i < computers[x].length; i++){
+            if(!visited[i] && computers[x][i] == 1){
+                dfs(i, computers);
             }
         }
+        
     }
 }
