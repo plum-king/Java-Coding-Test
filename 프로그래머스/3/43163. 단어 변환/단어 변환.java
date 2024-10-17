b589e8675cb297aa908c772f20e48a81580322dc
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-17 22:43:36 +0900

    [level 3] Title: 단어 변환, Time: 0.30 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,34 +1,37 @@
+import java.util.*;
+
 class Solution {
-    public static int answer = 0;
     public static boolean[] visited;
+    public static int answer = 0;
     public int solution(String begin, String target, String[] words) {
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i = 0; i < words.length; i++){
+            arr.add(words[i]);
+        }
         visited = new boolean[words.length];
         dfs(begin, target, words, 0);
-        
         return answer;
     }
     
-    public static void dfs(String begin, String target, String[] words, int count){
+    public static void dfs(String begin, String target, String[] words, int cnt){
         if(begin.equals(target)){
-            answer = count;
+            answer = cnt;
             return;
         }
+        
         for(int i = 0; i < words.length; i++){
-            if(visited[i]){
-                continue;
-            }
-            int k = 0; // 같은 스펠링 개수 세기
+            if(visited[i]) continue;
             
-            for(int j = 0; j < begin.length(); j++){
+            int check=0;
+            for(int j = 0; j < words[i].length(); j++){
                 if(begin.charAt(j) == words[i].charAt(j)){
-                    k++;
+                    check++;
                 }
-            }
-            
-            if(k == begin.length() - 1) { // 한 개 빼고 다 스펠링 수가 같다면
-                visited[i] = true;
-                dfs(words[i], target, words, count+1);
-                visited[i] = false;
+                if(check == begin.length()-1){
+                    visited[i] = true;
+                    dfs(words[i], target, words, cnt+1);
+                    visited[i] = false;
+                }
             }
         }
     }
