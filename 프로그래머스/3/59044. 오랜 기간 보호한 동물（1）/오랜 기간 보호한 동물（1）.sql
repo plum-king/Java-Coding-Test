7b515d55fa399e931f378aac9581b3ea8c3698b6
Jisoo Hwang <jeesoo0908@gmail.com>
2024-10-19 22:15:55 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
-SELECT ai.NAME, ai.DATETIME
-from ANIMAL_INS ai
-left join ANIMAL_OUTS ao
+SELECT ai.name, ai.datetime 
+from animal_ins ai
+left join animal_outs ao
 on ai.animal_id = ao.animal_id
 where ao.animal_id is null
-order by ai.DATETIME
+order by datetime
 limit 3;
